8f9bae42aaf7ad38cf25ca7328bbbc8f38d7e7f3
Long Do <longdo@dobtech.vn>
2021-10-02 20:37:25 +0700

    moved error and middlewares to common package

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,9 +1,8 @@
 import jwt from 'jsonwebtoken';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { validateRequest, BadRequestError } from '@ldxtickets/common';
 
-import { BadRequestError } from '../errors/bad-request-error';
-import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
 import { Password } from '../services/password';
 
